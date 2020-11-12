bcfef46a11f9fa450a4306638e98cd6b8c941cae
Johny-S <9364016@gmail.com>
2020-11-12 15:03:38 +0300

    Base structure

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+  return (
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">Todo list</h1>
+        <p class="lead">
+          Todo-react version <strong>1.0</strong>
+        </p>
+      </div>
+    </div>
+  )
+}
